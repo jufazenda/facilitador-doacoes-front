77a3684563314047b2ab2819b87bb37a9c203ac2
Victor Octavio <victoroctavio4@gmail.com>
2026-05-11 17:17:58 -0300

    Update assets to WebP format

--- src/utils/categoryImages.js
+++ src/utils/categoryImages.js
@@ -1,12 +1,12 @@
-import higiene from "../assets/images/category/higiene.png"
-import alimentos from "../assets/images/category/alimentos.png"
-import educacao from "../assets/images/category/educacao.png"
-import roupas from "../assets/images/category/roupas.png"
-import brinquedos from "../assets/images/category/brinquedos.png"
-import financeira from "../assets/images/category/financeira.png"
-import medicamentos from "../assets/images/category/medicamentos.png"
-import pet from "../assets/images/category/pet.png"
-import moradia from "../assets/images/category/moradia.png"
+import higiene from "../assets/images/category/higiene.webp"
+import alimentos from "../assets/images/category/alimentos.webp"
+import educacao from "../assets/images/category/educacao.webp"
+import roupas from "../assets/images/category/roupas.webp"
+import brinquedos from "../assets/images/category/brinquedos.webp"
+import financeira from "../assets/images/category/financeira.webp"
+import medicamentos from "../assets/images/category/medicamentos.webp"
+import pet from "../assets/images/category/pet.webp"
+import moradia from "../assets/images/category/moradia.webp"
 
 export const categoryImages = {
   higiene_cuidados_pessoais: higiene,
